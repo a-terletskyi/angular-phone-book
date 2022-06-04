5ff37f966e4c1b17268474e0eec60a4dc67d7f8d
aterletsky123 <terletsky28@gmail.com>
2022-06-04 21:16:46 +0300

    added events

--- src/app/phone-book/phone-book.component.ts
+++ src/app/phone-book/phone-book.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-phone-book',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 
 export class PhoneBookComponent implements OnInit {
+  addInput = '';
+  firstPlaceholder = 'first name goes here';
+  lastPlaceholder = 'second name goes here';
+  numberPlaceholder = 'number phone goes here';
+  firstNameInput!: string; secondNameInput = ''; numberPhoneInput!: string;
+  isModal!: boolean; phoneIndex!: number;
+  isValid = false; isEdit = false;
+
   contactsPhone: IPhone[] = [
     { firstName: 'Petya', lastName: 'Zhuk', phoneNumber: '0631111111' },
     { firstName: 'Petro', lastName: 'Petriv', phoneNumber: '0631222222' },
@@ -15,26 +24,62 @@ export class PhoneBookComponent implements OnInit {
     { firstName: 'Ira', lastName: 'Tytar', phoneNumber: '0636666666' },
     { firstName: 'Sofia', lastName: 'Zhuk', phoneNumber: '0637777777' },
   ];
-  inputValue = '';
 
   constructor() { }
   ngOnInit(): void { }
 
-  addPhone(input: HTMLInputElement): void { }
+  addPhone(): void {
+    this.firstNameInput = '';
+    this.secondNameInput = '';
+    this.numberPhoneInput = '';
+    this.isEdit = false;
+    this.isModal = true;
+  }
 
-  editPhone(index: number): void { }
+  deletePhone(index: number): void { this.contactsPhone.splice(index, 1) }
 
-  deletePhone(index: number): void { }
+  editPhone(index: number): void {
+    this.firstNameInput = this.contactsPhone[index].firstName;
+    this.secondNameInput = this.contactsPhone[index].lastName;
+    this.numberPhoneInput = this.contactsPhone[index].phoneNumber;
+    this.phoneIndex = index;
+    this.isEdit = true;
+    this.isModal = true;
+  }
 
-  sort(): void {} // це має бути pipe
+  savePhone(): void {
+    this.validity();
+    if (this.isValid) {
+      const phoneNum = { firstName: this.firstNameInput, lastName: this.secondNameInput, phoneNumber: this.numberPhoneInput };
+      this.isEdit ? this.contactsPhone.splice(this.phoneIndex, 1, phoneNum) : this.contactsPhone.push(phoneNum);
+      this.closeModal();
+    }
+  }
+
+  closeModal(): void { this.isModal = false }
+
+  validity(): void {
+    const firstNameRegExp: boolean = /^[a-zA-z]{2,16}$/.test(this.firstNameInput);
+    const lastNameRegExp: boolean = /^[a-zA-z]{2,16}$/.test(this.secondNameInput);
+    const phoneNumberREgExp: boolean = /^((\+38)?\(?\d{3}\)?[\s\.-]?(\d{7}|\d{3}[\s\.-]\d{2}[\s\.-]\d{2}|\d{3}-\d{4}))$/.test(this.numberPhoneInput)
+    if (this.firstNameInput && this.secondNameInput && this.numberPhoneInput) { this.isValid = true } else { this.isValid = false }
+
+    if (this.firstNameInput === '') { this.firstPlaceholder = 'Login cannot be blank' }
+    else if (!firstNameRegExp) { this.firstPlaceholder = 'Please provide a valid Login.' } else { this.firstPlaceholder = 'first name goes here' }
+
+    if (this.secondNameInput === '') { this.lastPlaceholder = 'Login cannot be blank' }
+    else if (!firstNameRegExp) { this.lastPlaceholder = 'Please provide a valid Login.' } else { this.lastPlaceholder = 'second name goes here' }
+
+    if (this.numberPhoneInput === '') { this.numberPlaceholder = 'Login cannot be blank' }
+    else if (!firstNameRegExp) { this.numberPlaceholder = 'Please provide a valid Login.' } else { this.numberPlaceholder = 'number phone goes here' }
+  }
+
+
+  sort(): void { } // це має бути pipe
 }
 
 interface IPhone { firstName: string, lastName: string, phoneNumber: string }
 
 // Plan
-// Створити модальне вікно, застилізувати його
-// Створити функцію, add яка буде добавляти елемент в масив
-// Створити функцію delete для видалення з масиву
-// Створити функцію edit, під час якої буде відкриватися модальне вікно (isModal = true), підставляємо дані з масиву в інпут поля
-// та потім замінюємо дані в елементі масиву
+// Створити валідацію для данних
 // СТворити pipe для сортування,без параментів змінна toggle буде робити asc або desc та добавлятиметься клас з іконкою, можна через псевдоелемент
